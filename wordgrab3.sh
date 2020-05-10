d542559d102eb432936085d303e2e3883236772d
Surendiran S <37256810+surendirans7777@users.noreply.github.com>
2020-05-10 14:31:14 +0530

    Update wordgrab3.sh

--- wordgrab3.sh
+++ wordgrab3.sh
@@ -2,7 +2,7 @@ wordgrab3() {
   echo "                             "
   echo "                             "
   echo "--------------------------------------"
-  echo "Enter the Particular URL of the Target"
+  echo "$(tput setaf 1)$(tput setab 2)Enter the Particular URL of the Target$(tput sgr 0)"
   echo "--------------------------------------"
   read url
   echo "                             "
@@ -10,7 +10,7 @@ wordgrab3() {
   echo "                             "
   echo "                             "
 
-  echo "Words along with the length"
+  echo "$(tput setaf 1)$(tput setab 7)Words along with the length$(tput sgr 0)"
   echo "---------------------------"
   tmpfile="$(date "+%s")"
   curl -sLk -m 3 $url | html2text | egrep -io "[0-9a-zA-Z\-]+" | tr '[:upper:]' '[:lower:]' | sed -r "s/^[^a-z]+//g" | sed -r "s/[^a-z0-9]+$//g" | sort -fu | tee -a $tmpfile | tr '-' '.'  | tee -a $tmpfile | tr "." "\n" >> $tmpfile
@@ -23,10 +23,16 @@ awk '{ print length(), $0 | "sort -n" }' output3.txt
 
 echo "                             "
 echo "                             "
-echo "Some Sensitive Keywords"
+echo "                             "
+echo "$(tput setaf 1)$(tput setab 7)Some Sensitive Keywords$(tput sgr 0)"
 echo "-----------------------"
 grep 'api\|key\|host\|token' output3.txt
 echo "                             "
 echo "                             "
+echo "                             "
 
-curl $url | grep 'api\|key\|host\|token'  
+
+echo "$(tput setaf 1)$(tput setab 7)Found Sensitive Items$(tput sgr 0)"
+echo "-----------------------"
+curl --silent $url | grep 'api\|key\|host\|token' 
+echo "                             "
