5f19396e59922da39e0bfaaecb30267980dbc24d
Surendiran S <37256810+surendirans7777@users.noreply.github.com>
2020-05-10 14:00:14 +0530

    Create wordgrab3.sh

---
+++ wordgrab3.sh
@@ -0,0 +1,28 @@
+wordgrab3() {
+  echo "                             "
+  echo "                             "
+  echo "--------------------------------------"
+  echo "Enter the Particular URL of the Target"
+  echo "--------------------------------------"
+  read url
+  echo "                             "
+  echo "                             "
+  echo "                             "
+  echo "                             "
+
+  echo "Words along with the length"
+  echo "---------------------------"
+  tmpfile="$(date "+%s")"
+  curl -sLk -m 3 $url | html2text | egrep -io "[0-9a-zA-Z\-]+" | tr '[:upper:]' '[:lower:]' | sed -r "s/^[^a-z]+//g" | sed -r "s/[^a-z0-9]+$//g" | sort -fu | tee -a $tmpfile | tr '-' '.'  | tee -a $tmpfile | tr "." "\n" >> $tmpfile
+  cat $tmpfile | sort -fu -o output3.txt
+  rm $tmpfile
+}
+
+wordgrab3
+awk '{ print length(), $0 | "sort -n" }' output3.txt 
+
+echo "                             "
+echo "                             "
+echo "Some Sensitive Keywords"
+echo "-----------------------"
+grep 'api\|key\|host' output3.txt 
