72b8465fa15df2388d7a1a96479f2976d80b60c3
Surendiran S <37256810+surendirans7777@users.noreply.github.com>
2020-05-10 15:39:48 +0530

    Update wordgrab3.sh

--- wordgrab3.sh
+++ wordgrab3.sh
@@ -34,6 +34,6 @@ echo "                             "
 
 echo "$(tput setaf 1)$(tput setab 7)Found Sensitive Items$(tput sgr 0)"
 echo "-----------------------"
-curl --silent $url | grep 'api\|key\|host\|token'
-curl --silent $url | grep 'api\|key\|host\|token'  > sensitive.txt
+curl --silent $url | sed -e 's/<[^>]*>//g' | grep --color=auto 'api\|key\|host\|token\|password\|dbuser\|secret\|access\|bucket\|redis\|root' 
+curl --silent $url | sed -e 's/<[^>]*>//g' | grep --color=auto 'api\|key\|host\|token\|password\|dbuser\|secret\|access\|bucket\|redis\|root'  > sensitive.txt
 echo "                             "
