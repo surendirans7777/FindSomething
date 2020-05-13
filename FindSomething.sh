8f00955b3f9f17c631cb486c21e935a8dec7e644
Surendiran S <37256810+surendirans7777@users.noreply.github.com>
2020-05-13 22:11:24 +0530

    Update FindSomething.sh

--- FindSomething.sh
+++ FindSomething.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 echo "                                                                            "
 echo "                                                                            "
-echo "------------------------------------------------------------------------------"
-echo "$(tput setaf 1)$(tput setab 7)Give me file that have list of urls and I will give you Sensitive Items output$(tput sgr 0)"
-echo "------------------------------------------------------------------------------"
+echo "----------------------------"
+echo "|                          |"
+echo "|       $(tput setaf 2)FindSomeThing$(tput sgr 0)      |"
+echo "|                          |"
+echo "----------------------------"
 echo "                                                                            "
 echo "                                                                            "
+echo "$(tput setaf 3)FileName: $(tput sgr 0)"
 read Filename
 echo "                                                                            "
 echo "                                                                            "
+echo "$(tput setaf 4)OUTPUT$(tput sgr 0)"
+echo " "
 while read LINE; do
-  curl -o /dev/null --silent --head --write-out "%{http_code} $LINE\n" "$LINE" 
-  curl -o /dev/null --silent --head --write-out "%{http_code} $LINE\n" "$LINE" | grep 200 >> output-status.txt && sed -i 's/200/ /' output-status.txt 
-
-
+  curl -o /dev/null --silent --head --write-out "%{http_code} || $LINE\n" "$LINE" 
   curl --silent $LINE > awes.txt 
-
-
 while IFS= read -r line; do
- grep -o -P --color=auto "$line.{0,100}.(';|',|\";|'|\")$" awes.txt
- grep -o -P --color=auto "$line.{0,100}.;$" awes.txt >> Output_Sensitive_items.txt
-
+ grep -o -P --color=auto "^.*$line.{0,100}.(';|',|\";|'|\")$"  awes.txt
 done < sensitive_keywords.txt
+echo "----------------------------------------------------------------------------"
 done < $Filename
-
